794e309b49c4046d58b523c34a1e7fc956e922bb
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-13 09:36:53 -0700

    Add Default Constructor

--- Patient.java
+++ Patient.java
@@ -1,5 +1,10 @@
 public class Patient {
     private int idNumber;
     private int age;
-    private BloodData bloodData=new BloodData();
+    private BloodData bloodData;
+    public Patient(){
+        this.idNumber=0;
+        this.age=0;
+        this.bloodData=new BloodData();
+    }
 }
