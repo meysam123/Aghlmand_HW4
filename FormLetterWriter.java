a0c1ec9a3bd4bbc286207f67ffafeccc90a509de
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-13 03:23:37 -0700

    Add the second overloded method displaySalutation

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -2,4 +2,7 @@ public class FormLetterWriter {
     public static void displaySalutation(String LastName){
         System.out.println("Dear Mr./Ms."+ LastName);
     }
+    public static void displaySalutation(String FirstName,String LastName){
+        System.out.println("Greeting Dear "+FirstName+" "+LastName);
+    }
 }
