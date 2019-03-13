95d2bf95b0cf0f1f20cc5ad71707f4c814751b19
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-13 09:05:03 -0700

    Add defualt Constructor

--- BloodData.java
+++ BloodData.java
@@ -1,5 +1,9 @@
 public class BloodData {
     private char bloodType;
     private boolean rhFactor;
-
+    public BloodData(){
+        this.bloodType='O';
+        this.rhFactor=true;
+    }
+    
 }
