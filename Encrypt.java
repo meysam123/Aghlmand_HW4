388e91dd38aa195644c60f42f405cb60219996aa
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-13 01:55:12 -0700

    Adding two set Methods

--- Encrypt.java
+++ Encrypt.java
@@ -1,4 +1,12 @@
 public class Encrypt {
     private String message;
     private int code;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
 }
