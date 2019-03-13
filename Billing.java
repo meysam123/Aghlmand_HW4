cbb5fb8db9619137c392bd66855b19e580fe11f2
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-13 08:14:15 -0700

    Add first  ComputeBill method

--- Billing.java
+++ Billing.java
@@ -1,4 +1,9 @@
+import java.sql.SQLOutput;
+
 public class Billing {
     public static final double tax=0.08;
-    
+    public static double computeBill(double price){
+        System.out.println("The Total due "+price*(1+Billing.tax));
+        return price*(1+Billing.tax);
+    }
 }
