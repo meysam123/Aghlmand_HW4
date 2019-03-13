65ef83bb7e7ec7d2ba4717f8d65db5a27a13b002
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-13 09:19:48 -0700

    Add ste Methods

--- BloodData.java
+++ BloodData.java
@@ -9,5 +9,15 @@ public class BloodData {
         this.bloodType=bloodType;
         this.rhFactor=rhFactor;
     }
-    
+
+    public void setBloodType(char bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public void setRhFactor(char rhFactor) {
+        if(rhFactor=='+')
+            this.rhFactor=true;
+        else if(rhFactor=='-')
+            this.rhFactor=false;
+    }
 }
