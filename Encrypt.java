ff939357fbcc018398fd2d591677dbbe24861315
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-13 02:38:37 -0700

    Add methods encypt & encriptAll

--- Encrypt.java
+++ Encrypt.java
@@ -9,4 +9,17 @@ public class Encrypt {
     public void setCode(int code) {
         this.code = code;
     }
+    public char encrypt(char x){
+        return (char)((int)x+this.code);
+    }
+    public String encryptAll(){
+        String result="";
+        for(int i=0;i<this.message.length();i++){
+            if(this.message.charAt(i)!=' ')
+                result=result+encrypt(this.message.charAt(i));
+            else
+                result=result+' ';
+        }
+        return result;
+    }
 }
