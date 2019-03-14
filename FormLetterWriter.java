d3cc499807e504e08bc54f8bacc71a50002f7a6e
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-14 09:22:18 -0700

    Fix Two methods of FormLetterWriter Class

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -1,8 +1,10 @@
 public class FormLetterWriter {
     public static void displaySalutation(String LastName){
         System.out.println("Dear Mr./Ms."+ LastName);
+        System.out.println("Thank you for your recent order");
     }
     public static void displaySalutation(String FirstName,String LastName){
         System.out.println("Greeting Dear "+FirstName+" "+LastName);
+        System.out.println("Thank you for your recent order");
     }
 }
