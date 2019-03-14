a5aeaf2342a1d4086a02b8bb971ff68f6ecc86fe
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-14 09:35:35 -0700

    create Main method to test the ComputeBill Class

--- Main.java
+++ Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test of First overloade Method:");
+        Billing.computeBill(1200);
+        System.out.println("\n");
+        System.out.println("Test of Second overloade Method:");
+        Billing.computeBill(1500,4);
+        System.out.println("\n");
+        System.out.println("Test of third overloade Method:");
+        Billing.computeBill(1000,5,200);
+        System.out.println("\n");
+    }
 }
