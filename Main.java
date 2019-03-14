1225708d66b5596ed3e51e61b1374fbb4cbb3ac9
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-14 09:13:48 -0700

    Set the Main method to test the Encrypt Class

--- Main.java
+++ Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        Encrypt test=new Encrypt();
+        test.setMessage("HI EVERYBODY! THIS IS A TEST");
+        test.setCode(5);
+        System.out.println(test.encryptAll());
+    }
 }
