0d4fb92a3a72d35ce320f67f0e7a1c5bef504b75
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-15 08:48:32 -0700

    Fix the Main method

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("result of the invoking first method:"+"\n");
         FormLetterWriter.displaySalutation("Ahmadi");
+        System.out.println("-------------------------------");
+        System.out.println("result of invoking the Second method:"+"\n");
         FormLetterWriter.displaySalutation("Alireza","Hedayati");
     }
 }
